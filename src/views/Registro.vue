9080f8d0800d1ca250b336ac22442a60d2fc2099
Melulo <sebasecund@gmail.com>
2025-08-15 22:29:33 -0300

    Registro

---
+++ src/views/Registro.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Registro de Usuario</h1>
+    <RegistroForm />
+  </div>
+</template>
+
+<script>
+import RegistroForm from '../components/RegistroForm.vue';
+export default { components: { RegistroForm } };
+</script>
