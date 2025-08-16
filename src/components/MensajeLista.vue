3169b790fb7f5779dca24a4cd31f2b30575652ea
Melulo <sebasecund@gmail.com>
2025-08-15 22:32:38 -0300

    MensajeLista

---
+++ src/components/MensajeLista.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Mensajes enviados</h2>
+    <ul>
+      <li v-for="(msg, i) in mensajes" :key="i">
+        <strong>{{ msg.titulo }}</strong> - para {{ msg.destinatario }}
+        <br />
+        {{ msg.contenido }}
+        <br />
+        <small>{{ msg.fecha }}</small>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { store } from '../store';
+export default {
+  computed: {
+    mensajes() {
+      return store.mensajes;
+    }
+  }
+};
+</script>
