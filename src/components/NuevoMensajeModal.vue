6ae3d0255d3d40acef143047553dcd316763df4d
Melulo <sebasecund@gmail.com>
2025-08-15 22:43:05 -0300

    NuevoMensajeModal

---
+++ src/components/NuevoMensajeModal.vue
@@ -0,0 +1,46 @@
+<template>
+  <Modal @close="$emit('close')">
+    <h3>Nuevo Mensaje</h3>
+    <div>
+      <label>Destinatario:</label>
+      <input v-model="form.destinatario" />
+    </div>
+    <div>
+      <label>Título:</label>
+      <input v-model="form.titulo" />
+    </div>
+    <div>
+      <label>Contenido:</label>
+      <textarea v-model="form.contenido"></textarea>
+    </div>
+    <button @click="enviar">Enviar</button>
+    <button @click="$emit('close')">Cancelar</button>
+  </Modal>
+</template>
+
+<script>
+import Modal from './Modal.vue';
+import { store } from '../store';
+
+export default {
+  components: { Modal },
+  data() {
+    return {
+      form: { destinatario: '', titulo: '', contenido: '' }
+    };
+  },
+  methods: {
+    enviar() {
+      if (!this.form.destinatario || !this.form.titulo || !this.form.contenido) {
+        alert('Todos los campos son obligatorios');
+        return;
+      }
+      store.mensajes.push({
+        ...this.form,
+        fecha: new Date().toLocaleString()
+      });
+      this.$emit('close');
+    }
+  }
+};
+</script>
