64a6c1aaf4505422fb299a0ca45f7703ae3d022f
Melulo <sebasecund@gmail.com>
2025-08-15 22:30:35 -0300

    Update UsuarioMensajes.vue

---
+++ src/views/UsuarioMensajes.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Mensajes</h1>
+    <MensajeLista />
+    <button @click="mostrarModal = true">Nuevo Mensaje</button>
+    <router-link to="/principal">Volver a Principal</router-link>
+
+    <NuevoMensajeModal v-if="mostrarModal" @close="mostrarModal = false" />
+  </div>
+</template>
+
+<script>
+import MensajeLista from '../components/MensajeLista.vue';
+import NuevoMensajeModal from '../components/NuevoMensajeModal.vue';
+
+export default {
+  components: { MensajeLista, NuevoMensajeModal },
+  data() {
+    return { mostrarModal: false };
+  }
+};
+</script>
